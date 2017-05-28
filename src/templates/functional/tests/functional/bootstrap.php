e54b982124feea8141cc3d2e850f30ae71237be7
insolita <webmaster100500@ya.ru>
2017-05-29 00:35:12 +0800

    fix functional path; add no db template

--- src/templates/functional/tests/functional/bootstrap.php
+++ src/templates/functional/tests/functional/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', realpath(__DIR__ . '/../../templates/'));
-$config = require(__DIR__ . '/../config/functional.php');
+defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', realpath(dirname(__DIR__ ).'/../' ));
+$config = require(dirname(__DIR__ ). '/config/functional.php');
 (new \yii\web\Application($config))->run();
